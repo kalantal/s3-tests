8c38538e6dc9f72e7bb36fe5be3abca1c1c33ecc
Justin Restivo <justinjrestivo@gmail.com>
2019-06-12 13:21:42 -0500

    Create install_certificates.sh

---
+++ scripts/install_certificates.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo cp Citi_DEVELOPMENT_chain.pem /etc/pki/ca-trust/source/
+sudo cp CitiInternalCAChain_LAB.pem /etc/pki/ca-trust/source/
+sudo cp CitiInternalCAChain_UAT.pem /etc/pki/ca-trust/source/
+sudo cp CitiInternalCAChain_PROD.pem /etc/pki/ca-trust/source/
+
+#sudo cp *.pem /etc/pki/ca-trust/source/
+
+cd /etc/pki/ca-trust/source/
+sudo update-ca-trust extract
