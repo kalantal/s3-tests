91595810a6566904ee1887a011eddbd0d86c682c
Justin Restivo <justinjrestivo@gmail.com>
2019-06-24 12:51:16 -0500

    Update and rename s3deletebuckets_1pass.sh to s3deletebuckets.sh

--- scripts/s3deletebuckets.sh
+++ scripts/s3deletebuckets.sh
@@ -7,19 +7,26 @@ if [ -e ~/.s3cfg ]
                 exit 0
 fi
 
-export vaultlist_=/tmp/s3vaultlist_
 export vaultlist=/tmp/s3vaultlist
 export prefix=s3tests-
 
 function gatherlist {
-s3cmd ls > $vaultlist_
-sed -ri 's .{18}  ' $vaultlist_
-cat $vaultlist_ | grep $prefix &> $vaultlist
+	s3cmd ls | awk '{print $3}' | grep $prefix > $vaultlist
 }
 gatherlist && echo -en '\nLsit of vaults:\n' && cat $vaultlist
 
+echo -en '\nDeleting items\n'
+function deleteitems {
+#delete items inside vaults
+cat $vaultlist | while read line ; do s3cmd del --recursive --force $line ; done
+}
+deleteitems
+
+#delete vaults
 echo -en '\nDeleting vaults\n'
 function deletevaults {
         cat $vaultlist | while read line ; do s3cmd rb --recursive --force $line ; done
 }
 deletevaults
+
+gatherlist
