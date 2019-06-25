9f4f5b70f85effa66769f0c1f15d6f02cd0607b3
Justin Restivo <justinjrestivo@gmail.com>
2019-06-24 19:26:11 -0500

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -18,7 +18,6 @@ credentials_secret_key=$(echo "$secret_key" | sed "s/secret_key/aws_secret_acces
 echo "[default]" > credentials
 echo $credentials_access_key >> credentials
 echo $credentials_secret_key >> credentials
-
 if [ ! -f credentials ]; then
 	echo "credentials build error, exiting" && exit 0
 fi
@@ -28,16 +27,13 @@ cleanup_access_key=$(echo "$access_key" | sed "s/access_key/id/")
 cleanup_secret_key=$(echo "$secret_key" | sed "s/secret_key/key/")
 echo $cleanup_access_key > cleanupKeys
 echo $cleanup_secret_key >> cleanupKeys
-
 if [ ! -f cleanupKeys ]; then
 	echo "cleanupKeys build error, exiting" && exit 0
 fi
 
 DATE=$(date +%Y-%m-%d_%H%M)
-
 LOG_DIR=output/$DATE
 mkdir -p $LOG_DIR
-
 test_cmd="S3TEST_CONF=s3.conf ./virtualenv/bin/nosetests -v --with-xunit --xunit-file=$LOG_DIR/nosetests.xml --with-blacklist --blacklist-file=blacklists/blacklist.txt"
 
 (
@@ -70,5 +66,4 @@ test_cmd="S3TEST_CONF=s3.conf ./virtualenv/bin/nosetests -v --with-xunit --xunit
 eval $test_cmd 2>&1 | tee -a $LOG_DIR/output.log
 
 perl scripts/parse-nose.pl -i $LOG_DIR/nosetests.xml -o $LOG_DIR/nosetests.csv
-
 sed -ri '/teardown/d' $LOG_DIR/nosetests.csv
