7ad964f63bc41124f9eabf27b83561501870a431
kalantal <justinjrestivo@gmail.com>
2019-06-28 19:25:44 -0500

    s3delete

--- scripts/s3deletebuckets.sh
+++ scripts/s3deletebuckets.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
 
-export vaultlist=/tmp/vaultlist
+export itemList=/tmp/itemList
+export vaultList=/tmp/vaultList
 export prefix=s3tests-
+
+echo -en "s3deletebuckets.sh\n"
 
 if [ -e ~/.s3cfg ]
   then echo -en "\ns3cfg found, continuing..\n"
@@ -10,23 +13,30 @@ if [ -e ~/.s3cfg ]
     exit 0
 fi
 
-function gatherlist {
-  s3cmd ls | awk '{print $3}' | grep $prefix > $vaultlist
+function gatherlist-vaults {
+  s3cmd ls | awk '{print $3}' | grep $prefix > $vaultList
 }
-gatherlist && echo -en '\nLsit of vaults:\n' && cat $vaultlist
+gatherlist-vaults && echo -en '\nLsit of items in vaults:\n' && cat $vaultList
+
+function gatherlist-items {
+  s3cmd la | awk '{print $4}' | grep $prefix > $itemList
+}
+gatherlist-items && echo -en '\nLsit of items in vaults:\n' && cat $itemList
 
 echo -en '\nDeleting items:\n'
 # Delete items inside vaults
 function deleteitems {
-  cat $vaultlist | while read line ; do s3cmd del --recursive --force $line ; done
+  cat $itemList | while read line ; do s3cmd del --recursive --force $line -v ; done
 }
 deleteitems
 
 echo -en '\nDeleting vaults:'
 # Delete vaults
 function deletevaults {
-  cat $vaultlist | while read line ; do s3cmd rb --recursive --force $line ; done
+  cat $vaultList | while read line ; do s3cmd rb --recursive --force $line -v ; done
 }
 deletevaults
+
+echo
 
 exit 0
