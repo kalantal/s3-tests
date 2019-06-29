d13b2bcca61dd98b30486330a3e87f23f1df2ecd
Justin Restivo <justinjrestivo@gmail.com>
2019-06-29 02:24:38 -0500

    Update s3wipe.sh

--- scripts/s3wipe.sh
+++ scripts/s3wipe.sh
@@ -4,10 +4,8 @@ source cleanupKeys
 export vaultlist=/tmp/vaultlist
 export prefix=s3tests-
 
-if [ -e ~/.s3cfg ]
-  then echo -en "\ns3cfg found, continuing..\n"
-    else echo -en "\ns3cfg not found..\n"
-    exit 0
+if [ ! -f ~/.s3cfg ]; then
+	echo "s3cmd not found, exiting" && exit 0
 fi
 
 function gatherlist {
