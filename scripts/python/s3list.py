4de40ea5afb23eb7599009cdda50a4e28f958328
Justin Restivo <justinjrestivo@gmail.com>
2019-06-30 01:01:38 -0500

    Update s3list.py

--- scripts/python/s3list.py
+++ scripts/python/s3list.py
@@ -8,6 +8,7 @@ def main():
     def enumerate_s3_buckets():
         print "Bucket list:"
         s3 = boto3.resource('s3')
+        endpoint_url = 's3.amazonaws.com',
         for bucket in s3.buckets.all():
              print "s3://" + ("{}".format(bucket.name))
     enumerate_s3_buckets()
@@ -17,6 +18,7 @@ def main():
     def enumerate_s3_objects():
         print "Object list:"
         s3 = boto3.resource('s3')
+        endpoint_url = 's3.amazonaws.com',
         for bucket in s3.buckets.all():
              for object in bucket.objects.all():
                  print "s3://" + ("{}".format(object.bucket_name)) + "/" + ("{}".format(object.key))
