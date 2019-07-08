6902161d73825da20dabac07384392e2b7997d72
Justin Restivo <justinjrestivo@gmail.com>
2019-07-08 14:53:47 -0400

    Update s3objects.py

--- scripts/python/s3objects.py
+++ scripts/python/s3objects.py
@@ -9,9 +9,9 @@ os.environ['REQUESTS_CA_BUNDLE'] = '/etc/ssl/certs/ca-bundle.crt'
 def main():
     def enumerate_s3_objects():
         s3 = boto3.resource('s3',
-             aws_access_key_id='kEA8AdJU4u3PKZxdq1Ae',
-             aws_secret_access_key='72vJM62gIjW6PJD03YJvjmyBCKXkWr9sqoNVKYLu',
-             endpoint_url='https://mwdc-plt-obj-wip1.nam.nsroot.net')
+             aws_access_key_id='',
+             aws_secret_access_key='',
+             endpoint_url='https://')
         for bucket in s3.buckets.all():
              for object in bucket.objects.all():
                  print "s3://" + ("{}".format(object.bucket_name)) + "/" + ("{}".format(object.key))
@@ -21,9 +21,9 @@ if __name__ == '__main__':
     main()
 
     s3 = boto3.client('s3',   
-            aws_access_key_id='kEA8AdJU4u3PKZxdq1Ae',
-            aws_secret_access_key='72vJM62gIjW6PJD03YJvjmyBCKXkWr9sqoNVKYLu',
-            endpoint_url='https://mwdc-plt-obj-wip1.nam.nsroot.net')
+            aws_access_key_id='',
+            aws_secret_access_key='',
+            endpoint_url='https://')
     response = s3.list_buckets()
 
     #print('Existing buckets:')
