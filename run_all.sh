693c2f1515453454e0f84191d898ece9844bd1b4
Justin Restivo <justinjrestivo@gmail.com>
2019-07-09 21:11:12 -0500

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -58,6 +58,7 @@ sed -ri '/teardown/d' "$LOG_DIR"/nosetests.csv
 (
   echo -en "Cleanup/n"
   bash scripts/s3deletebuckets.sh
+  bash scripts/s3deletebuckets.sh
   echo -en "\nRemaining Vaults:\n"
   s3cmd ls | awk '{print $3}' | grep $prefix
 ) >> "$LOG_DIR"/output.log
